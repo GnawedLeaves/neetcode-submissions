82cbcc7f4e722e730bc5d10d1fdaa48fc5e5db68
Marcel <111361529+GnawedLeaves@users.noreply.github.com>
2026-08-05 17:00:39 +0800

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        // i htink its a left to right thing
+        const n = nums.length;
+        let result = new Array<number>(n);
+        result[0] = 1;
+
+        for (let i = 1; i < n; i++) {
+            result[i] = nums[i - 1] * result[i - 1];
+        }
+
+
+        let suffix = 1
+        for (let j = n - 1; j >= 0; j--) {
+            result[j] = result[j] * suffix
+            suffix *= nums[j]
+        }
+
+
+        return result;
+    }
+}
