f10a8118d61f747f5ceee5fd9d51e71c1291236b
Marcel <111361529+GnawedLeaves@users.noreply.github.com>
2026-09-02 00:03:57 +0800

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        const str = s.trim().split(" ").join("");
+        const cleaned = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+       
+
+
+        for (let i = 0, j = cleaned.length - 1; i <= j; j--, i++) {
+            console.log(cleaned[i], cleaned[j]);
+            if (cleaned[i] !== cleaned[j]) return false;
+        }
+
+        return true;
+    }
+}
